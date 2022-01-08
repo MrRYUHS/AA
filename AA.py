72f47f4f57f4b944bd331973e4e225613b156a1d
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-01-09 06:19:32 +0900

    파이썬 언어 비쥬얼 스튜디오

--- AA.py
+++ AA.py
@@ -1,13 +1,17 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-n, k=map(int,input().split())
-a=list(map(int, input().split()))
-res=set() #중복을 제거한다
-for i in range(n):
-    for j in range(i+1, n):
-        for m in range(j+1, n):
-            res.add(a[i]+a[j]+a[m])
-res=list(res)
-res.sort(reverse=True)
-print(res[k-1])
+n=int(input())
+a=list(map(int,input().split()))
+ave=round(sum(a)/n)
+min=2147000000
+for idx, x in enumerate(a):
+    tmp=abs(x-ave)
+    if tmp<min:
+        min=tmp
+        score=x
+        res=idx+1
+    elif tmp==min:
+        if x>score:
+            res=idx+1
+print(ave, res)
