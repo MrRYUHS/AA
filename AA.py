b8ecc72af38736b95b25ec5fbb84dcb4212d8264
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-01-10 22:08:38 +0900

    파이썬 언어 기초

--- AA.py
+++ AA.py
@@ -3,15 +3,15 @@ sys.stdin=open("input.txt", "rt")
 
 n=int(input())
 a=list(map(int,input().split()))
-ave=round(sum(a)/n)
+av=round(sum(a)/n)
 min=2147000000
 for idx, x in enumerate(a):
-    tmp=abs(x-ave)
+    tmp=abs(x-av)
     if tmp<min:
         min=tmp
         score=x
-        res=idx+1
+        r=idx
     elif tmp==min:
         if x>score:
-            res=idx+1
-print(ave, res)
+            r=idx+1
+print(av, r)
