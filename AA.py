e2f5b908892934134a60c7f67dda3f00db5944ba
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-01-11 21:57:39 +0900

    파이썬 기초

--- AA.py
+++ AA.py
@@ -1,17 +1,13 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-n=int(input())
+n, k=map(int,input().split())
 a=list(map(int,input().split()))
-av=round(sum(a)/n)
-min=2147000000
-for idx, x in enumerate(a):
-    tmp=abs(x-av)
-    if tmp<min:
-        min=tmp
-        score=x
-        r=idx
-    elif tmp==min:
-        if x>score:
-            r=idx+1
-print(av, r)
+res=set()
+for i in range(n):
+    for j in range(i+1, n):
+        for m in range(j+1, n):
+            res.add(a[i]+a[j]+a[m])
+y=list(res)
+y.sort(reverse=True)
+print(y[k-1])
