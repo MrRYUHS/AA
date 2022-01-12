63a306f15a05dda436f83ccfb3b6222004860ca1
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-01-13 00:09:36 +0900

    파이썬 언어 기초

--- AA.py
+++ AA.py
@@ -1,15 +1,13 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-n, m=map(int,input().split())
-cnt=[0]*(n+m+3)
-max=-2147000000
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        cnt[i+j]+=1
-for i in range(n+m+1):
-    if cnt[i]>max:
-        max=cnt[i]
-for i in range(n+m+1):
-    if cnt[i]==max:
-        print(i, end='')
+n,k=map(int,input().split())
+a=list(map(int,input().split()))
+res=set()
+for i in range(n):
+    for j in range(i+1,n):
+        for l in range(j+1,n):
+            res.add(a[i]+a[j]+a[l])
+res=list(res)
+res.sort(reverse=True)
+print(res[k-1])
