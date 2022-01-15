d98e95867fcfb9dc0c93a38fa09d1975f23931ce
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-01-15 21:30:30 +0900

    파이썬 언어 기초

--- AA.py
+++ AA.py
@@ -2,11 +2,25 @@ import sys
 sys.stdin=open("input.txt", "rt")
 
 n=int(input())
-ch=[0]*(n+1)
-cnt=0
-for i in range(2,n+1):
-    if ch[i]==0:
-        cnt+=1
-        for j in range(i, n+1, i):
-            ch[j]=1
-print(cnt)
+a=list(map(int,input().split()))
+
+def reverse(x):
+    res=0
+    while x>0:
+        t=x%10
+        res=res*10+t
+        x=x//10
+    return res
+def isPrime(x):
+    if x==1:
+        return False
+    for i in range(2, x//2+1):
+        if x%i==0:
+            return False
+    else:
+        return True
+
+for x in a:
+    tot=reverse(x)
+    if isPrime(tot):
+        print(tot)
