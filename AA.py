a0a176deeb35490b7a99651afedb8e2c319ed4f8
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-01-17 00:00:48 +0900

    파이썬 언어

--- AA.py
+++ AA.py
@@ -1,14 +1,6 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-n=int(input())
-a=list(map(int,input().split()))
-sum=0
-cnt=0
-for i in a:
-    if i==1:
-        cnt+=1
-        sum+=cnt
-    else:
-        cnt=0
-print(sum)
+n=str(input())
+for i in range(n):
+    print(i)
