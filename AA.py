201fc8f441bbd0947087d25947add46801f6fb5b
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-01-17 00:21:39 +0900

    파이썬 언어

--- AA.py
+++ AA.py
@@ -1,6 +1,14 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-n=str(input())
+n=int(input())
 for i in range(n):
-    print(i)
+    s=input()
+    s=s.upper()
+    size=len(s)
+    for j in range(size//2):
+        if s[j]!=s[-1-j]:
+            print("#%d NO" %(i+1))
+            break
+    else:
+        print("#%d YES" %(i+1))
