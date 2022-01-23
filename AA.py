782f6e7c7204b27da790024e6623ab0d8b281ad1
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-01-24 00:42:10 +0900

    파이썬 언어

--- AA.py
+++ AA.py
@@ -3,36 +3,15 @@ sys.stdin=open("input.txt", "rt")
 
 n=int(input())
 a=[list(map(int,input().split())) for _ in range(n)]
-
-sum=0
-ans=-1
+res=0
+s=e=n//2
 for i in range(n):
-  for j in range(n):
-    sum+=a[i][j]
-  if sum>ans:
-    ans=sum
-  sum=0
-
-for i in range(n):
-  for j in range(n):
-    sum+=a[j][i]
-  if sum>ans:
-    ans=sum
-  sum=0
-
-i=0
-for j in range(n-1,-1,-1):
-  sum+=a[i][j]
-  i+=1
-if sum>ans:
-  ans=sum
-sum=0
-
-i=0
-for j in range(n):
-  sum+=a[i][j]
-  i+=1
-if sum>ans:
-  ans=sum
-
-print(ans)
+    for j in range(s, e+1):
+        res+=a[i][j]
+    if i<n//2:
+        s-=1
+        e+=1
+    else:
+        s+=1
+        e-=1
+print(res)
