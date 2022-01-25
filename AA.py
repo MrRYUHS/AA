a72bd42acf81a657af5c3e5b1cff858795b09be8
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-01-25 18:35:05 +0900

    파이썬 언어 기초

--- AA.py
+++ AA.py
@@ -1,28 +1,27 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-n=int(input())
-a=[list(map(int, input().split())) for _ in range(n)]
-m=int(input())
-for i in range(m):
-    h, t, k=map(int, input().split())
-    if t==0:
-        for _ in range(k):
-            a[h-1].append(a[h-1].pop(0))
-    else:
-        for _ in range(k):
-            a[h-1].insert(0, a[h-1].pop())
+def check(a):
+    for i in range(9):
+        ch1=[0]*10
+        ch2=[0]*10
+        for j in range(9):
+            ch1[a[i][j]]=1
+            ch2[a[j][i]]=1
+        if sum(ch1)!=9 or sum(ch2)!=9:
+            return False
+    for i in range(3):
+        for j in range(3):
+            ch3=[0]*10
+            for k in range(3):
+                for s in range(3):
+                    ch3[a[i*3+k][j*3+s]]=1
+            if sum(ch3)!=9:
+                return False
+    return True
 
-res=0
-s=0
-e=n-1
-for i in range(n):
-    for j in range(s,e+1):
-        res+=a[i][j]
-    if i<n//2:
-        s+=1
-        e-=1
-    else:
-        s-=1
-        e+=1
-print(res)
+a=[list(map(int, input().split())) for _ in range(9)]
+if check(a):
+    print("YES")
+else:
+    print("NO")
