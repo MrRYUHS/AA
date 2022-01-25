d0368a528c5996609670c6d80effbd97f09ba9b6
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-01-26 00:22:06 +0900

    파이썬 언어 기초

--- AA.py
+++ AA.py
@@ -1,27 +1,17 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-def check(a):
-    for i in range(9):
-        ch1=[0]*10
-        ch2=[0]*10
-        for j in range(9):
-            ch1[a[i][j]]=1
-            ch2[a[j][i]]=1
-        if sum(ch1)!=9 or sum(ch2)!=9:
-            return False
-    for i in range(3):
-        for j in range(3):
-            ch3=[0]*10
-            for k in range(3):
-                for s in range(3):
-                    ch3[a[i*3+k][j*3+s]]=1
-            if sum(ch3)!=9:
-                return False
-    return True
-
-a=[list(map(int, input().split())) for _ in range(9)]
-if check(a):
-    print("YES")
-else:
-    print("NO")
+a=[list(map(int, input().split())) for _ in range(7)]
+cnt=0
+len=5
+for i in range(3):
+    for j in range(7):
+        tmp=a[j][i:i+len]
+        if tmp==tmp[::-1]:
+            cnt+=1
+        for k in range(len//2):
+            if a[i+k][j]!=a[len+i-k-1][j]:
+                break
+        else:
+            cnt+=1
+print(cnt)
