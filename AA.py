8aa23657c3e5de29ad76156323ad3f04ed95b021
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-02-07 22:10:11 +0900

    파이썬 언어

--- AA.py
+++ AA.py
@@ -1,17 +1,26 @@
 import sys
 sys.stdin=open("input.txt", "rt")
+def Count(len):
+    cnt=0
+    for x in Line:
+        cnt+=(x//len)
+    return cnt
 
-a=[list(map(int, input().split())) for _ in range(7)]
-cnt=0
-len=5
-for i in range(3):
-    for j in range(7):
-        tmp=a[j][i:i+len]
-        if tmp==tmp[::-1]:
-            cnt+=1
-        for k in range(len//2):
-            if a[i+k][j]!=a[len+i-k-1][j]:
-                break
-        else:
-            cnt+=1
-print(cnt)
+k,n=map(int,input().split())
+Line=[]
+res=0
+largest=0
+for i in range(k):
+    tmp=int(input())
+    Line.append(tmp)
+    largest=max(largest, tmp)
+lt=1
+rt=largest
+while lt<=rt:
+    mid=(lt+rt)//2
+    if Count(mid)>=n:
+        res=mid
+        lt=mid+1
+    else:
+        rt=mid-1
+print(res)
