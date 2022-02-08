aa26e536f043ac83df493cb8d115911da346e560
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-02-08 23:55:20 +0900

    파이썬 언어

--- AA.py
+++ AA.py
@@ -1,26 +1,27 @@
 import sys
 sys.stdin=open("input.txt", "rt")
-def Count(len):
-    cnt=0
-    for x in Line:
-        cnt+=(x//len)
+def Count(capacity):
+    cnt=1
+    sum=0
+    for x in Music:
+        if sum+x>capacity:
+            cnt+=1
+            sum=x
+        else:
+            sum+=x
     return cnt
 
-k,n=map(int,input().split())
-Line=[]
-res=0
-largest=0
-for i in range(k):
-    tmp=int(input())
-    Line.append(tmp)
-    largest=max(largest, tmp)
+n,m=map(int,input().split())
+Music=list(map(int,input().split()))
+maxx=max(Music)
 lt=1
-rt=largest
+rt=sum(Music)
+res=0
 while lt<=rt:
     mid=(lt+rt)//2
-    if Count(mid)>=n:
+    if mid>=maxx and Count(mid)<=m:
         res=mid
-        lt=mid+1
-    else:
         rt=mid-1
+    else:
+        lt=mid+1
 print(res)
