ec2090f39f5bfaaaee4e1cb5ee50f14483983312
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-02-10 22:04:38 +0900

    파이썬 뮤직비디오

--- AA.py
+++ AA.py
@@ -1,27 +1,26 @@
 import sys
 sys.stdin=open("input.txt", "rt")
-
-def Count(len):
-    cnt=0
-    for i in Line:
-        cnt+=(i//len)
+def Count(capa):
+    cnt=1
+    sum=0
+    for i in Music:
+        if sum+i>capa:
+            cnt+=1
+            sum=i
+        else:
+            sum+=i
     return cnt
 
-k, n=map(int,input().split())
-Line=[]
-res=0
-largest=0
-for i in range(k):
-    tmp=int(input())
-    Line.append(tmp)
-    largest=max(largest, tmp)
+n, m=map(int,input().split())
+Music=list(map(int,input().split()))
 lt=1
-rt=largest
+rt=sum(Music)
+res=0
 while lt<=rt:
     mid=(lt+rt)//2
-    if Count(mid)>=n:
+    if Count(mid)<=m:
         res=mid
-        lt=mid+1
-    else:
         rt=mid-1
+    else:
+        lt=mid+1
 print(res)
