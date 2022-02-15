7128a10f4f29a6f2996ed2ad80707b94f941dd14
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-02-16 03:33:55 +0900

    증가수열 만들기 그리디

--- AA.py
+++ AA.py
@@ -1,12 +1,28 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-L=int(input())
+n=int(input())
 a=list(map(int,input().split()))
-M=int(input())
-a.sort()
-for _ in range(M):
-    a[0]+=1
-    a[L-1]-=1
-    a.sort()
-print(a[L-1]-a[0])
+lt=0
+rt=n-1
+last=0
+res=""
+tmp=[]
+while lt<=rt:
+    if a[lt]>last:
+        tmp.append((a[lt], 'L'))
+    if a[rt]>last:
+        tmp.append((a[rt], 'R'))
+    tmp.sort()
+    if len(tmp)==0:
+        break
+    else:
+        res+=tmp[0][1]
+        last=tmp[0][0]
+        if tmp[0][1]=='L':
+            lt+=1
+        else:
+            rt-=1
+    tmp.clear()
+print(len(res))
+print(res)
