dfc92544752427d34460e2184497625f56bbf701
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-02-15 22:30:52 +0900

    창고정리 그리디

--- AA.py
+++ AA.py
@@ -1,16 +1,12 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-n=int(input())
-py=[]
-for i in range(n):
-    h,w=map(int,input().split())
-    py.append((h,w))
-py.sort(reverse=True)
-aw=0
-cnt=0
-for h,w in py:
-    if w>aw:
-        aw=w
-        cnt+=1
-print(cnt)
+L=int(input())
+a=list(map(int,input().split()))
+M=int(input())
+a.sort()
+for _ in range(M):
+    a[0]+=1
+    a[L-1]-=1
+    a.sort()
+print(a[L-1]-a[0])
