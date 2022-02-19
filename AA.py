d5b496bd1ea20f1a659b8850bf816fcd31aa3234
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-02-20 03:07:46 +0900

    파이썬 후위표기식(스택)

--- AA.py
+++ AA.py
@@ -3,14 +3,25 @@ sys.stdin=open("input.txt", "rt")
 
 s=input()
 stack=[]
-cnt=0
-for i in range(len(s)):
-    if s[i]=='(':
-        stack.append(s[i])
+res=''
+for i in s:
+    if i.isdecimal():
+        res+=i
     else:
-        stack.pop()
-        if s[i-1]=='(':
-            cnt+=len(stack)
-        else:
-            cnt+=1
-print(cnt)
+        if i=='(':
+            stack.append(i)
+        elif i=='*' or i=='/':
+            while stack and (stack[-1]=='*' or stack[-1]=='/'):
+                res+=stack.pop()
+            stack.append(i)
+        elif i=='+' or i=='-':
+            while stack and stack[-1]!='(':
+                res+=stack.pop()
+            stack.append(i)
+        elif i==')':
+            while stack and stack[-1]!='(':
+                res+=stack.pop()
+            stack.pop()
+while stack:
+    res+=stack.pop()
+print(res)
