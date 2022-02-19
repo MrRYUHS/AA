3474360e2bb101189dade93a2900ccca8a44f386
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-02-19 16:56:48 +0900

    파이썬 가장큰수

--- AA.py
+++ AA.py
@@ -1,16 +1,15 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-n=int(input())
-a=list(map(int,input().split()))
-a.insert(0,0)
-seq=[0]*n
-for i in range(1, n+1):
-    for j in range(n):
-        if a[i]==0 and seq[j]==0:
-            seq[j]=i
-            break
-        elif seq[j]==0:
-            a[i]-=1
-for x in seq:
-    print(x, end=' ')
+num,m=map(int,input().split())
+num=list(map(int,str(num)))
+stack=[]
+for i in num:
+    while stack and m>0 and stack[-1]<i:
+        stack.pop()
+        m-=1
+    stack.append(i)
+if m!=0:
+    stack=stack[:-m]
+res=''.join(map(str,stack))
+print(res)
