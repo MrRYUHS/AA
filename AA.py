35c1d8b8416142b399f375992de67cd1029a701e
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-02-22 22:11:34 +0900

    후위식 변환

--- AA.py
+++ AA.py
@@ -3,25 +3,24 @@ sys.stdin=open("input.txt", "rt")
 
 s=input()
 stack=[]
-res=''
 for i in s:
     if i.isdecimal():
-        res+=i
+        stack.append(int(i))
     else:
-        if i=='(':
-            stack.append(i)
-        elif i=='*' or i=='/':
-            while stack and (stack[-1]=='*' or stack[-1]=='/'):
-                res+=stack.pop()
-            stack.append(i)
-        elif i=='+' or i=='-':
-            while stack and stack[-1]!='(':
-                res+=stack.pop()
-            stack.append(i)
-        elif i==')':
-            while stack and stack[-1]!='(':
-                res+=stack.pop()
-            stack.pop()
-while stack:
-    res+=stack.pop()
-print(res)
+        if i=="*":
+            a=stack.pop()
+            b=stack.pop()            
+            stack.append(b*a)            
+        elif i=="/":
+            a=stack.pop()
+            b=stack.pop()            
+            stack.append(b/a)
+        elif i=="+":
+            a=stack.pop()
+            b=stack.pop()
+            stack.append(b+a)
+        elif i=="-":
+            a=stack.pop()
+            b=stack.pop()
+            stack.append(b-a)
+print(stack[0])
