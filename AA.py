f4c80f513882b969f4e768b85d3320896f463ce2
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-02-27 21:14:21 +0900

    연습

--- AA.py
+++ AA.py
@@ -2,18 +2,6 @@ import sys
 from collections import deque
 sys.stdin=open("input.txt", "rt")
 
-need=input()
-n=int(input())
-for i in range(n):
-    plan=input()
-    dq=deque(need)
-    for x in plan:
-        if x in dq:
-            if x!=dq.popleft():
-                print("#%d NO" %(i+1))
-                break
-    else:
-        if len(dq)==0:
-            print("#%d YES" %(i+1))
-        else:
-            print("#%d NO" %(i+1))
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+print(n,m,a)
