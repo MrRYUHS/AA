a48d0977873cc7e8b5f6634ddfeaa2d2614842b7
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-03-19 21:09:19 +0900

    최소힙

--- AA.py
+++ AA.py
@@ -1,23 +1,16 @@
 import sys
+import heapq as hq
 sys.stdin=open("input.txt", "rt")
 
-a=input()
-b=input()
-str1=[0]*52
-str2=[0]*52
-for x in a:
-    if x.isupper():
-        str1[ord(x)-65]+=1
-    else:
-        str1[ord(x)-71]+=1
-for x in b:
-    if x.isupper():
-        str2[ord(x)-65]+=1
-    else:
-        str2[ord(x)-71]+=1
-for i in range(52):
-    if str1[i]!=str2[i]:
-        print("NO")
+a=[]
+while True:
+    n=int(input())
+    if n==-1:
         break
-else:
-    print("YES")
+    if n==0:
+        if len(a)==0:
+            print(-1)
+        else:
+            print(hq.heappop(a))
+    else:
+        hq.heappush(a,n)
