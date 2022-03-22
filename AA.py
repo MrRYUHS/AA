0df3db212dacbeac3bf8db61491e0bf1fe0aa8c7
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-03-22 23:40:00 +0900

    practice

--- AA.py
+++ AA.py
@@ -1,15 +1,23 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-a=[]
-while True:
-    n=int(input())
-    if n==-1:
-        break
-    if n==0:
-        if len(a)==0:
-            print(-1)
-        else:
-            print(-hq.heappop(a))
+a=input()
+b=input()
+str1=[0]*52
+str2=[0]*52
+for x in a:
+    if x.isupper():
+        str1[ord(x)-65]+=1
     else:
-        hq.heappush(a,-n)
+        str1[ord(x)-71]+=1
+for x in b:
+    if x.isupper():
+        str2[ord(x)-65]+=1
+    else:
+        str2[ord(x)-71]+=1
+for i in range(52):
+    if str1[i]!=str2[i]:
+        print("NO")
+        break
+else:
+    print("YES")
