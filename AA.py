e047aacdc33df7299e6109d1b24edf11504d355b
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-03-24 23:31:08 +0900

    practice

--- AA.py
+++ AA.py
@@ -1,23 +1,15 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-a=input()
-b=input()
-str1=[0]*52
-str2=[0]*52
-for x in a:
-    if x.isupper():
-        str1[ord(x)-65]+=1
-    else:
-        str1[ord(x)-71]+=1
-for x in b:
-    if x.isupper():
-        str2[ord(x)-65]+=1
-    else:
-        str2[ord(x)-71]+=1
-for i in range(52):
-    if str1[i]!=str2[i]:
-        print("NO")
-        break
-else:
-    print("YES")
+num, m=map(int, input().split())
+num=list(map(int, str(num)))
+stack=[]
+for x in num:
+    while stack and m>0 and stack[-1]<x:
+        stack.pop()
+        m-=1
+    stack.append(x)
+if m!=0:
+    stack=stack[:-m]
+res=''.join(map(str, stack))
+print(res)
