6898315299fb22c209a9999965f934cef471c8d4
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-03-26 23:34:48 +0900

    이진트리순회

--- AA.py
+++ AA.py
@@ -1,12 +1,10 @@
-import sys
-sys.stdin=open("input.txt", "rt")
-
-def DFS(x):
-    if x==0:
+def DFS(v):
+    if v>7:
         return
-    else:
-        DFS(x//2)
-        print(x%2, end='')
-
-n=int(input())
-DFS(n)
+    else:  
+        print(v, end=' ')
+        DFS(v*2)
+        DFS(v*2+1)
+        
+if __name__=="__main__":
+    DFS(1)
