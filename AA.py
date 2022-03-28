dd5a5f2d36bf661afd1f6f83925630fa984e331e
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-03-28 21:33:31 +0900

    practice

--- AA.py
+++ AA.py
@@ -1,10 +1,14 @@
-def DFS(v):
-    if v>7:
-        return
-    else:  
-        print(v, end=' ')
-        DFS(v*2)
-        DFS(v*2+1)
-        
-if __name__=="__main__":
-    DFS(1)
+import sys
+sys.stdin=open("input.txt", "rt")
+num, m=map(int, input().split())
+num=list(map(int, str(num)))
+stack=[]
+for x in num:
+    while stack and m>0 and stack[-1]<x:
+        stack.pop()
+        m-=1
+    stack.append(x)
+if m!=0:
+    stack=stack[:-m]
+res=''.join(map(str, stack))
+print(res)
