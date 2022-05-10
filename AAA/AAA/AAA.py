fcdd94081f5e9639edb0ee99936254bd4b843018
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-05-10 21:34:20 +0900

    practice

--- AAA/AAA/AAA.py
+++ AAA/AAA/AAA.py
@@ -1,13 +1,10 @@
 import sys
 sys.stdin=open("input.txt", "r")
+def DFS(x):
+    if x>0:        
+        DFS(x-1)
+        print(x, end=" ")
 
-n, k=map(int, input().split())
-a=list(map(int, input().split()))
-res=set()
-for i in range(n):
-    for j in range(i+1, n):
-        for m in range(j+1, n):
-            res.add(a[i]+a[j]+a[m])
-res=list(res)
-res.sort(reverse=True)
-print(res[k-1])
+
+n=int(input())
+DFS(n)
