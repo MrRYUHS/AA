3d87d2ab47589bf6f587818b5638332926be1e76
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-05-15 20:07:32 +0900

    재귀함수 이진수출력

--- AAA/AAA/AAA.py
+++ AAA/AAA/AAA.py
@@ -1,10 +1,12 @@
 import sys
 sys.stdin=open("input.txt", "r")
 def DFS(x):
-    if x>0:        
-        DFS(x-1)
-        print(x, end=" ")
-
-
-n=int(input())
-DFS(n)
+    if x==0:
+        return
+    else:
+        
+        DFS(x//2)
+        print(x%2, end=" ")
+if __name__=="__main__":
+    n=int(input())
+    DFS(n)
