a451c9d3ae687c423bad10686f0f346bd2a7bea2
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-05-19 14:26:50 +0900

    연습

--- AAA/AAA/AAA.py
+++ AAA/AAA/AAA.py
@@ -3,9 +3,9 @@ sys.stdin=open("input.txt", "r")
 def DFS(v):
     if v>7:
         return
-    else:
-        print(v, end='')
+    else:        
         DFS(v*2)
         DFS(v*2+1)
+        print(v, end=' ')
 if __name__=="__main__":
     DFS(1)
