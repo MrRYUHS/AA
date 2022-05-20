4baf01b877435bcba511c40c32e19a662de3f2dd
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-05-20 21:30:36 +0900

    부분집합 구하기

--- AAA/AAA/AAA.py
+++ AAA/AAA/AAA.py
@@ -1,11 +1,17 @@
 import sys
 sys.stdin=open("input.txt", "r")
 def DFS(v):
-    if v>7:
-        return
-    else:        
-        DFS(v*2)
-        DFS(v*2+1)
-        print(v, end=' ')
+    if v==n+1:
+        for i in range(1, n+1):
+            if ch[i]==1:
+                print(i, end=' ')
+        print()
+    else:
+        ch[v]=1
+        DFS(v+1)
+        ch[v]=0
+        DFS(v+1)
 if __name__=="__main__":
+    n=int(input())
+    ch=[0]*(n+1)
     DFS(1)
