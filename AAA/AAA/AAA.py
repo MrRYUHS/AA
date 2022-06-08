7ae19cbb9056f4a65181e98e903494ab99f10c50
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-06-08 21:34:54 +0900

    부분집합

--- AAA/AAA/AAA.py
+++ AAA/AAA/AAA.py
@@ -1,17 +1,19 @@
 import sys
 sys.stdin=open("input.txt", "r")
-def DFS(v):
-    if v==n+1:
-        for i in range(1, n+1):
-            if ch[i]==1:
-                print(i, end=' ')
-        print()
+def DFS(L, sum):
+    if sum>total//2:
+        return
+    if L==n:
+        if sum==(total-sum):
+            print("YES")
+            sys.exit(0)
     else:
-        ch[v]=1
-        DFS(v+1)
-        ch[v]=0
-        DFS(v+1)
+        DFS(L+1, sum+a[L])
+        DFS(L+1, sum)
+
 if __name__=="__main__":
     n=int(input())
-    ch=[0]*(n+1)
-    DFS(1)
+    a=list(map(int, input().split()))
+    total=sum(a)
+    DFS(0,0)
+    print("NO")
